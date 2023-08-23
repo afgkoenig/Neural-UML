b7285f434881fdb39bb5609b906e0e81ed8a9316
afgkoenig <119843483+afgkoenig@users.noreply.github.com>
2023-08-23 16:20:46 +0200

    Update view_annotation.py

--- datasetA/view_annotation.py
+++ datasetA/view_annotation.py
@@ -19,21 +19,22 @@ liste_data = ["navigable","inheritance","realization","dependency","aggregation"
 with open("./datasetA.csv", newline='') as csvfile:
         filereader = csv.reader(csvfile, delimiter=';')
         for row in filereader:
-            for elt in os.listdir("./test/"):
-                if row[1]==elt:
-                    img = Image.open(f"./test/{elt}")
-                    img1 = ImageDraw.Draw(img)
-                    annotation = row[3:]
-                    try:
-                        while len(annotation)>=5:
-                            shape = [(float(annotation[0])/2,float(annotation[1])/2),(float(annotation[2])/2,float(annotation[3])/2)]
-                            img1.rectangle(shape,outline=liste_color[int(annotation[4])],width = 3)
-                            img1.text((float(annotation[0])/2+1,float(annotation[1])/2+1),text = liste_data[int(annotation[4])] , fill = liste_color[int(annotation[4])])
-                            annotation = annotation[5:]
-
-                        if img.mode == "RGBA":
-                            img.save(f"./results/{elt[:-4]}.png")
-                        else:
-                            img = img.convert("RGB")
-                            img.save(f"./results/{elt}")
-                    except: ValueError
+            if row[2] == "Yes":
+                for elt in os.listdir("./test/"):
+                    if row[1]==elt:
+                        img = Image.open(f"./test/{elt}")
+                        img1 = ImageDraw.Draw(img)
+                        annotation = row[3:]
+                        try:
+                            while len(annotation)>=5:
+                                shape = [(float(annotation[0])/2,float(annotation[1])/2),(float(annotation[2])/2,float(annotation[3])/2)]
+                                img1.rectangle(shape,outline=liste_color[int(annotation[4])],width = 3)
+                                img1.text((float(annotation[0])/2+1,float(annotation[1])/2+1),text = liste_data[int(annotation[4])] , fill = liste_color[int(annotation[4])])
+                                annotation = annotation[5:]
+    
+                            if img.mode == "RGBA":
+                                img.save(f"./results/{elt[:-4]}.png")
+                            else:
+                                img = img.convert("RGB")
+                                img.save(f"./results/{elt}")
+                        except: ValueError
